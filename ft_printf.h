502a5f44018d65e1b824be80ee643d990b50819b
StevenKim <stevenkim@Steven.local>
2020-04-18 12:55:08 +0900

    edit for normcheck

--- ft_printf.h
+++ ft_printf.h
@@ -34,18 +34,18 @@ typedef struct	s_struct
 	int			precision;
 }				t_struct;
 
-int			ft_printf(const char *s, ...);
-int			checkformat(const char *s, va_list ap);
+int				ft_printf(const char *s, ...);
+int				checkformat(const char *s, va_list ap);
 
-void		ifpercent(t_struct *f);
-void		ifchar(t_struct *f, va_list ap);
-void		ifstring(t_struct *f, va_list ap);
-void		ifinteger(t_struct *f, va_list ap, int isunsigned);
-void		ifhex(t_struct *f, va_list ap);
-void		ifpointer(t_struct *f, va_list ap);
+void			ifpercent(t_struct *f);
+void			ifchar(t_struct *f, va_list ap);
+void			ifstring(t_struct *f, va_list ap);
+void			ifinteger(t_struct *f, va_list ap, int isunsigned);
+void			ifhex(t_struct *f, va_list ap);
+void			ifpointer(t_struct *f, va_list ap);
 
-void		displayzero(t_struct *f, int num);
-void		displaywidth(t_struct *f);
-void		putflaginstar(t_struct *f, va_list ap);
+void			displayzero(t_struct *f, int num);
+void			displaywidth(t_struct *f);
+void			putflaginstar(t_struct *f, va_list ap);
 
 #endif
